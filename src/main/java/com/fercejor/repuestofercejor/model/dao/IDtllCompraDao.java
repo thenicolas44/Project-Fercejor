4966201926b1a1360fca801290add9dbab1cb833
thenicolas44 <nicolas_d_12@hotmail.com>
2023-11-17 18:39:11 -0500

    eliminacion del detalle compra

--- src/main/java/com/fercejor/repuestofercejor/model/dao/IDtllCompraDao.java
+++
@@ -1,9 +0,0 @@
-package com.fercejor.repuestofercejor.model.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.fercejor.repuestofercejor.model.entity.DtllCompraEntity;
-
-public interface IDtllCompraDao extends CrudRepository<DtllCompraEntity,String>{
-    
-}
