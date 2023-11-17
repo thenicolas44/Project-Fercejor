14dc1e6429b6a29fbd83a7395b31a8ec4a4b10d6
Marcos Romero <-e>
2023-11-17 16:41:42 -0500

    Cambios en la rama marck terminados

--- src/main/resources/static/js/ventasSweet.js
+++ src/main/resources/static/js/ventasSweet.js
@@ -3,15 +3,20 @@
 document.querySelector('#tablaVentas').addEventListener('submit', function (event) {
     // Evita que el formulario se envíe
     console.log("Hola mundo");
-    let name = document.getElementById('ventasNombre');
-    let descripcion = document.getElementById('ventasDescrippcion');
-    let producto = document.getElementById('ventasProducto');
-    let precio = document.getElementById('ventasPrecio');
-    let cantidad = document.getElementById('ventasCantidad');
-    let precioTotal = document.getElementById('ventasTotal');
+    let name = document.getElementById('AventasNombre');
+    let ape = document.getElementById('AventasApellido');
+    let email = document.getElementById('AventasCorreo');
+    let dir = document.getElementById('AventasDireccion');
+    let tel = document.getElementById('AventasTelefono');
+    let pais = document.getElementById('AventasPais');
+    let empleado = document.getElementById('AventasEmpleado');
+    let producto = document.getElementById('AventasProducto');
+    let precio = document.getElementById('AventasPrecio');
+    let cantidad = document.getElementById('AventasCantidad');
 
 
-    if(name.value === '' || descripcion.value === '' || producto.value === '' || precio.value === '' || cantidad.value === '') {
+    if(name.value === '' || ape.value === '' || email.value === '' || dir.value === ''
+        || tel.value === ''|| pais.value === '' || empleado.value === '' || producto.value === '' || precio.value === ''|| cantidad.value === '') {
         event.preventDefault(); // Evita que el formulario se envíe
         Swal.fire(
             'Campos vacíos',
@@ -23,7 +28,7 @@ document.querySelector('#tablaVentas').addEventListener('submit', function (even
         event.preventDefault(); // Evita que el formulario se envíe
         Swal.fire({
             title: 'Buen trabajo',
-            text: 'EL empleado ha sido registrado',
+            text: 'La venta ha sido registrada',
             icon:'success',
             showCancelButton: false,
             confirmButtonText: 'OK',
@@ -39,15 +44,20 @@ document.querySelector('#tablaVentas').addEventListener('submit', function (even
 document.querySelector('#AtablaVentas').addEventListener('submit', function (event) {
     // Evita que el formulario se envíe
     console.log("Hola mundo");
-    let name = document.getElementById('AventasNombre');
-    let descripcion = document.getElementById('AventasDescrippcion');
-    let producto = document.getElementById('AventasProducto');
-    let precio = document.getElementById('AventasPrecio');
-    let cantidad = document.getElementById('AventasCantidad');
-    let precioTotal = document.getElementById('AventasTotal');
+    let name = document.getElementById('EventasNombre');
+    let ape = document.getElementById('EventasApellido');
+    let email = document.getElementById('EventasCorreo');
+    let dir = document.getElementById('EventasDireccion');
+    let tel = document.getElementById('EventasTelefono');
+    let pais = document.getElementById('EventasPais');
+    let empleado = document.getElementById('EventasEmpleado');
+    let producto = document.getElementById('EventasProducto');
+    let precio = document.getElementById('EventasPrecio');
+    let cantidad = document.getElementById('EventasCantidad');
 
 
-    if(name.value === '' || descripcion.value === '' || producto.value === '' || precio.value === '' || cantidad.value === '') {
+    if(name.value === '' || ape.value === '' || email.value === '' || dir.value === ''
+        || tel.value === ''|| pais.value === '' || empleado.value === '' || producto.value === '' || precio.value === ''|| cantidad.value === '') {
         event.preventDefault(); // Evita que el formulario se envíe
         Swal.fire(
             'Campos vacíos',
@@ -59,7 +69,7 @@ document.querySelector('#AtablaVentas').addEventListener('submit', function (eve
         event.preventDefault(); // Evita que el formulario se envíe
         Swal.fire({
             title: 'Buen trabajo',
-            text: 'EL empleado ha sido registrado',
+            text: 'La venta ha sido actualizada',
             icon:'success',
             showCancelButton: false,
             confirmButtonText: 'OK',
