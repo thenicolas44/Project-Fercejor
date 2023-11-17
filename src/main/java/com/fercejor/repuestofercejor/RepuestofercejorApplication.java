96c16a885432f8ad0f0929b9dc9434491725367d
thenicolas44 <nicolas_d_12@hotmail.com>
2023-11-16 23:46:02 -0500

    Se agrega el Proyecto Fercejor

---
+++ src/main/java/com/fercejor/repuestofercejor/RepuestofercejorApplication.java
@@ -0,0 +1,13 @@
+package com.fercejor.repuestofercejor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepuestofercejorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepuestofercejorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fercejor/repuestofercejor/RepuestofercejorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fercejor.repuestofercejor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepuestofercejorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
