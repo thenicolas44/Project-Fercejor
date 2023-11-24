d0836eb1cd4910ab1654cc5bfded7b153651e701
thenicolas44 <nicolas_d_12@hotmail.com>
2023-11-24 14:50:02 -0500

    listar clientes

--- src/main/java/com/fercejor/repuestofercejor/controller/ClienteController.java
+++ src/main/java/com/fercejor/repuestofercejor/controller/ClienteController.java
@@ -1,13 +1,21 @@
 package com.fercejor.repuestofercejor.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.fercejor.repuestofercejor.model.service.inteface.IClienteService;
 
 @Controller
 @RequestMapping("/clientes")
 public class ClienteController {
+    @Autowired
+    private IClienteService clienteService;
+
     @RequestMapping("/")
-    public String inicio(){
+    public String inicio(Model modelo){
+        modelo.addAttribute("listaCliente", clienteService.listarCliente());
         return "administrador/clientes";
     }
 }
