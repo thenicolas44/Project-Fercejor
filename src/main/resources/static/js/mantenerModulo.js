78836834ccef25790a0a9315c11a1e880dbe8913
thenicolas44 <nicolas_d_12@hotmail.com>
2023-11-24 21:28:26 -0500

    categoria editar

--- src/main/resources/static/js/mantenerModulo.js
+++ src/main/resources/static/js/mantenerModulo.js
@@ -2,7 +2,9 @@
     var listaBontonesEditar = document.querySelectorAll(".editarCategoria");
     listaBontonesEditar.forEach(item =>{
         item.addEventListener("click", e =>{
-
+            document.getElementById('idCate').value = item.dataset.id;
+            document.getElementById('nombres4').value = item.dataset.nombre;
+            document.getElementById('descripcion4').value = item.dataset.descripcion;
             new bootstrap.Modal(document.getElementById('modalEditarCategoria')).show();
         })
     })
