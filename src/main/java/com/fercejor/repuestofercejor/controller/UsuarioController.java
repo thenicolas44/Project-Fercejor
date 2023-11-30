a86154dd7559c37a597087e7932fca9597911da5
thenicolas44 <nicolas_d_12@hotmail.com>
2023-11-30 01:15:29 -0500

    registrar Empleado y cargo

--- src/main/java/com/fercejor/repuestofercejor/controller/UsuarioController.java
+++ src/main/java/com/fercejor/repuestofercejor/controller/UsuarioController.java
@@ -1,5 +1,7 @@
 package com.fercejor.repuestofercejor.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -22,11 +24,47 @@ public class UsuarioController {
         EmpleadoEntity empleado = new EmpleadoEntity();
         CargoEntity cargo = new CargoEntity();
 
-        modelo.addAttribute("emplado", empleado);
+        modelo.addAttribute("empleado", empleado);
         modelo.addAttribute("cargo", cargo);
 
         modelo.addAttribute("listaEmpleado", empleadoService.listaEmpleado());
         modelo.addAttribute("listaCargo", cargoService.listaCargo());
         return "administrador/usuarios";
+    }
+    @RequestMapping("/guardarCargo")
+    public String guardarCargo(CargoEntity cargo){
+        List<CargoEntity> listaCargo = cargoService.listaCargo();
+        if(listaCargo.size() == 0){
+            String id_cargo = "CARG0001";
+            cargo.setIdCargo(id_cargo);
+        }
+        else{
+            CargoEntity ultimoCargo = listaCargo.get(listaCargo.size()-1);
+            String ultimoID = ultimoCargo.getIdCargo();
+            int numero = Integer.parseInt(ultimoID.substring(4));
+            numero++;
+            String nuevoID = String.format("CARG%04d", numero);
+            cargo.setIdCargo(nuevoID);
+        }
+        cargoService.guardarCargo(cargo);
+        return "redirect:/usuarios/";
+    }
+    @RequestMapping("/guardarEmpleado")
+    public String guardarEmpleado(EmpleadoEntity empleado){
+        List<EmpleadoEntity> listaEmpleado = empleadoService.listaEmpleado();
+        if(listaEmpleado.size() == 0){
+            String id_empleado = "EMPL0001";
+            empleado.setIdEmpleado(id_empleado);
+        }
+        else{
+            EmpleadoEntity ultimoEmpleado = listaEmpleado.get(listaEmpleado.size()-1);
+            String ultimoId = ultimoEmpleado.getIdEmpleado();
+            int num = Integer.parseInt(ultimoId.substring(4));
+            num++;
+            String nuevoId = String.format("EMPL%04d", num);
+            empleado.setIdEmpleado(nuevoId);
+        }
+        empleadoService.guardarEmpleado(empleado);
+        return "redirect:/usuarios/";
     }
 }
