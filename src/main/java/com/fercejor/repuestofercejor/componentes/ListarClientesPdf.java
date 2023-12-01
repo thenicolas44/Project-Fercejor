7907071fe1b02c9108df892f74a6a965eda829b9
thenicolas44 <nicolas_d_12@hotmail.com>
2023-11-30 21:56:32 -0500

    pdf de venta y compra amigable

--- src/main/java/com/fercejor/repuestofercejor/componentes/ListarClientesPdf.java
+++ src/main/java/com/fercejor/repuestofercejor/componentes/ListarClientesPdf.java
@@ -3,11 +3,19 @@ package com.fercejor.repuestofercejor.componentes;
 import java.util.List;
 import java.util.Map;
 
+import java.awt.Color;
+
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.view.document.AbstractPdfView;
 
 import com.fercejor.repuestofercejor.model.entity.CbzCompraEntity;
 import com.lowagie.text.Document;
+import com.lowagie.text.Element;
+import com.lowagie.text.FontFactory;
+import com.lowagie.text.PageSize;
+import com.lowagie.text.Phrase;
+import com.lowagie.text.Font;
+import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
 
@@ -21,7 +29,85 @@ public class ListarClientesPdf extends AbstractPdfView{
     protected void buildPdfDocument(Map<String, Object> model, Document document, PdfWriter writer,
             HttpServletRequest request, HttpServletResponse response) throws Exception {
                 List<CbzCompraEntity> listarCompras = (List<CbzCompraEntity>) model.get("listadoCompras");
+                //fuentes y tamaños 
+                Font fuenteTitulo = FontFactory.getFont(FontFactory.HELVETICA_BOLD,20,Color.GREEN);
+                Font fuenteTituloColumnas = FontFactory.getFont(FontFactory.HELVETICA_BOLD,12,Color.GREEN);
+                Font fuenteDataCeldas = FontFactory.getFont(FontFactory.COURIER,10,Color.BLACK);
+
+                document.setPageSize(PageSize.LETTER.rotate());
+                document.setMargins(-20, -20, 30, 20);
+                document.open();
+
+                PdfPCell celda = null;
+
+                //tabla para el titulo del pdf
+                PdfPTable tablaTitulo = new PdfPTable(1);
+
+                celda = new PdfPCell(new Phrase("LISTADO GENERAL DE COMPRAS", fuenteTitulo));
+                celda.setBorder(0);
+                celda.setBackgroundColor(new Color(255,247,0));
+                celda.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
+                celda.setVerticalAlignment(PdfPCell.ALIGN_CENTER);
+                celda.setPadding(30);
+
+                tablaTitulo.addCell(celda);
+                tablaTitulo.setSpacingAfter(30);
+                //tabla para el listado de compras
                 PdfPTable tablaCompras = new PdfPTable(7);
+                
+                tablaCompras.setWidths(new float[] {0.8f, 2f, 2f, 3.5f, 1.5f,1.5f,1.5f});
+
+                celda = new PdfPCell(new Phrase("ID", fuenteTituloColumnas));
+                celda.setBackgroundColor(Color.lightGray);
+                celda.setHorizontalAlignment(Element.ALIGN_CENTER);
+                celda.setVerticalAlignment(Element.ALIGN_CENTER);
+                celda.setPadding(10);
+                tablaCompras.addCell(celda);
+
+                celda = new PdfPCell(new Phrase("FECHA", fuenteTituloColumnas));
+                celda.setBackgroundColor(Color.lightGray);
+                celda.setHorizontalAlignment(Element.ALIGN_CENTER);
+                celda.setVerticalAlignment(Element.ALIGN_CENTER);
+                celda.setPadding(10);
+                tablaCompras.addCell(celda);
+
+
+                celda = new PdfPCell(new Phrase("PROVEEDOR", fuenteTituloColumnas));
+                celda.setBackgroundColor(Color.lightGray);
+                celda.setHorizontalAlignment(Element.ALIGN_CENTER);
+                celda.setVerticalAlignment(Element.ALIGN_CENTER);
+                celda.setPadding(10);
+                tablaCompras.addCell(celda);
+
+                celda = new PdfPCell(new Phrase("PRODUCTO", fuenteTituloColumnas));
+                celda.setBackgroundColor(Color.lightGray);
+                celda.setHorizontalAlignment(Element.ALIGN_CENTER);
+                celda.setVerticalAlignment(Element.ALIGN_CENTER);
+                celda.setPadding(10);
+                tablaCompras.addCell(celda);
+
+                celda = new PdfPCell(new Phrase("CANTIDAD", fuenteTituloColumnas));
+                celda.setBackgroundColor(Color.lightGray);
+                celda.setHorizontalAlignment(Element.ALIGN_CENTER);
+                celda.setVerticalAlignment(Element.ALIGN_CENTER);
+                celda.setPadding(10);
+                tablaCompras.addCell(celda);
+
+                celda = new PdfPCell(new Phrase("PRECIO", fuenteTituloColumnas));
+                celda.setBackgroundColor(Color.lightGray);
+                celda.setHorizontalAlignment(Element.ALIGN_CENTER);
+                celda.setVerticalAlignment(Element.ALIGN_CENTER);
+                celda.setPadding(10);
+                tablaCompras.addCell(celda);
+
+                celda = new PdfPCell(new Phrase("TOTAL", fuenteTituloColumnas));
+                celda.setBackgroundColor(Color.lightGray);
+                celda.setHorizontalAlignment(Element.ALIGN_CENTER);
+                celda.setVerticalAlignment(Element.ALIGN_CENTER);
+                celda.setPadding(10);
+                tablaCompras.addCell(celda); 
+
+    
                 listarCompras.forEach(compras -> {
                     tablaCompras.addCell(compras.getIdCbzCompra());
                     tablaCompras.addCell(compras.getFechaCbzCompra().toString());
@@ -32,7 +118,7 @@ public class ListarClientesPdf extends AbstractPdfView{
                     tablaCompras.addCell(compras.getTotalCbzCompra().toString());
                     
                 });
-
+            document.add(tablaTitulo);
             document.add(tablaCompras);
     }
     
--- src/main/java/com/fercejor/repuestofercejor/componentes/ListarVentasPdf.java
+++ src/main/java/com/fercejor/repuestofercejor/componentes/ListarVentasPdf.java
@@ -3,11 +3,20 @@ package com.fercejor.repuestofercejor.componentes;
 import java.util.List;
 import java.util.Map;
 
+import java.awt.Color;
+
+
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.view.document.AbstractPdfView;
 
 import com.fercejor.repuestofercejor.model.entity.CbzVentaEntity;
 import com.lowagie.text.Document;
+import com.lowagie.text.Element;
+import com.lowagie.text.FontFactory;
+import com.lowagie.text.PageSize;
+import com.lowagie.text.Phrase;
+import com.lowagie.text.Font;
+import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
 
@@ -21,7 +30,133 @@ public class ListarVentasPdf extends AbstractPdfView{
     protected void buildPdfDocument(Map<String, Object> model, Document document, PdfWriter writer,
             HttpServletRequest request, HttpServletResponse response) throws Exception {
                 List<CbzVentaEntity> listarVentas = (List<CbzVentaEntity>) model.get("listadoVenta");
+
+                //Fuentes y tamaños
+                Font fuenteTitulo = FontFactory.getFont(FontFactory.HELVETICA_BOLD,20,Color.BLUE);
+                Font fuenteTituloColumnas = FontFactory.getFont(FontFactory.HELVETICA_BOLD,12,Color.BLUE);
+                Font fuenteDataCeldas = FontFactory.getFont(FontFactory.COURIER,10,Color.BLACK);
+
+                document.setPageSize(PageSize.LETTER.rotate());
+                document.setMargins(-20, -20, 30, 20);
+                document.open();
+
+                PdfPCell celda = null;
+
+                //tabla para el titulo pdf
+                PdfPTable tablaTitulo = new PdfPTable(1);
+
+                celda = new PdfPCell(new Phrase("LISTADO GENERAL DE VENTAS", fuenteTitulo));
+                celda.setBorder(0);
+                celda.setBackgroundColor(new Color(158,72,217));
+                celda.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
+                celda.setVerticalAlignment(PdfPCell.ALIGN_CENTER);
+                celda.setPadding(30);
+
+                tablaTitulo.addCell(celda);
+                tablaTitulo.setSpacingAfter(30);
+                //tabla para el listado de ventas
+
                 PdfPTable tablaVentas = new PdfPTable(8);
+                
+                tablaVentas.setWidths(new float[] {0.8f, 2f, 2f, 1.5f, 3.5f, 2f,1.5f,1.5f});
+
+                celda = new PdfPCell(new Phrase("ID", fuenteTituloColumnas));
+                celda.setBackgroundColor(Color.lightGray);
+                celda.setHorizontalAlignment(Element.ALIGN_CENTER);
+                celda.setVerticalAlignment(Element.ALIGN_CENTER);
+                celda.setPadding(10);
+                tablaVentas.addCell(celda);
+
+                celda = new PdfPCell(new Phrase("FECHA", fuenteTituloColumnas));
+                celda.setBackgroundColor(Color.lightGray);
+                celda.setHorizontalAlignment(Element.ALIGN_CENTER);
+                celda.setVerticalAlignment(Element.ALIGN_CENTER);
+                celda.setPadding(10);
+                tablaVentas.addCell(celda);
+
+
+                celda = new PdfPCell(new Phrase("CLIENTE", fuenteTituloColumnas));
+                celda.setBackgroundColor(Color.lightGray);
+                celda.setHorizontalAlignment(Element.ALIGN_CENTER);
+                celda.setVerticalAlignment(Element.ALIGN_CENTER);
+                celda.setPadding(10);
+                tablaVentas.addCell(celda);
+
+                celda = new PdfPCell(new Phrase("EMPLEADO", fuenteTituloColumnas));
+                celda.setBackgroundColor(Color.lightGray);
+                celda.setHorizontalAlignment(Element.ALIGN_CENTER);
+                celda.setVerticalAlignment(Element.ALIGN_CENTER);
+                celda.setPadding(10);
+                tablaVentas.addCell(celda);
+
+                celda = new PdfPCell(new Phrase("PRODUCTO", fuenteTituloColumnas));
+                celda.setBackgroundColor(Color.lightGray);
+                celda.setHorizontalAlignment(Element.ALIGN_CENTER);
+                celda.setVerticalAlignment(Element.ALIGN_CENTER);
+                celda.setPadding(10);
+                tablaVentas.addCell(celda);
+
+                celda = new PdfPCell(new Phrase("CANTIDAD", fuenteTituloColumnas));
+                celda.setBackgroundColor(Color.lightGray);
+                celda.setHorizontalAlignment(Element.ALIGN_CENTER);
+                celda.setVerticalAlignment(Element.ALIGN_CENTER);
+                celda.setPadding(10);
+                tablaVentas.addCell(celda);
+
+                celda = new PdfPCell(new Phrase("PRECIO", fuenteTituloColumnas));
+                celda.setBackgroundColor(Color.lightGray);
+                celda.setHorizontalAlignment(Element.ALIGN_CENTER);
+                celda.setVerticalAlignment(Element.ALIGN_CENTER);
+                celda.setPadding(10);
+                tablaVentas.addCell(celda);
+
+                celda = new PdfPCell(new Phrase("TOTAL", fuenteTituloColumnas));
+                celda.setBackgroundColor(Color.lightGray);
+                celda.setHorizontalAlignment(Element.ALIGN_CENTER);
+                celda.setVerticalAlignment(Element.ALIGN_CENTER);
+                celda.setPadding(10);
+                tablaVentas.addCell(celda); 
+                
+                for (CbzVentaEntity ventas : listarVentas) {
+                    celda = new PdfPCell(new Phrase(ventas.getIdCbzVenta(),fuenteDataCeldas));
+                    celda.setPadding(5);
+                    tablaVentas.addCell(celda);
+
+                    celda = new PdfPCell(new Phrase(ventas.getFechaCbzVenta().toString()));
+                    celda.setPadding(5);
+                    tablaVentas.addCell(celda);
+
+                    celda = new PdfPCell(new Phrase(ventas.getClienteCbzVenta().getNombreCliente()));
+                    celda.setPadding(5);
+                    tablaVentas.addCell(celda);
+
+                    celda = new PdfPCell(new Phrase(ventas.getEmpleadoCbzVenta().getPrimerNombreEmpleado()));
+                    celda.setPadding(5);
+                    tablaVentas.addCell(celda);
+
+                    celda = new PdfPCell(new Phrase(ventas.getProductoCbzVenta().getNombreProducto()));
+                    celda.setPadding(5);
+                    tablaVentas.addCell(celda);
+
+                    celda = new PdfPCell(new Phrase(ventas.getCantidadCbzVenta().toString()));
+                    celda.setPadding(5);
+                    tablaVentas.addCell(celda);
+
+                    celda = new PdfPCell(new Phrase(ventas.getPrecioCbzVenta().toString()));
+                    celda.setPadding(5);
+                    tablaVentas.addCell(celda);
+
+                    celda = new PdfPCell(new Phrase(ventas.getTotalCbzVenta().toString()));
+                    celda.setPadding(5);
+                    tablaVentas.addCell(celda);
+
+                }
+
+
+                /* 
+                
+
+                
                 listarVentas.forEach(ventas -> {
                     tablaVentas.addCell(ventas.getIdCbzVenta());
                     tablaVentas.addCell(ventas.getFechaCbzVenta().toString());
@@ -32,6 +167,9 @@ public class ListarVentasPdf extends AbstractPdfView{
                     tablaVentas.addCell(ventas.getPrecioCbzVenta().toString());
                     tablaVentas.addCell(ventas.getTotalCbzVenta().toString());
                 });
+                */
+                /* */
+            document.add(tablaTitulo);
             document.add(tablaVentas);
     }
     
